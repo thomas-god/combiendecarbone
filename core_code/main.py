c23155e7326d05e99cc816761d3b1687da7b1750
Thomas <thomas@MacBook-Pro-de-Thomas.local>
2019-03-31 12:59:10 +0200

    [TG] GES: add Pie chart graph to sum-up ges emisions.

--- core_code/main.py
+++ core_code/main.py
@@ -2,6 +2,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
 import json
 
 
@@ -68,11 +69,15 @@ app.layout = html.Div(
         html.Div(id='output-container-button',
                 children='Enter a value and press submit')
     ]
+    + [html.Div(id='graph_container', children=dcc.Graph(id='ges_graph'))]
 )
 
 
 @app.callback(
-    dash.dependencies.Output('output-container-button', 'children'),
+    [dash.dependencies.Output('output-container-button', 'children'),
+    dash.dependencies.Output('graph_container', 'style'),
+    dash.dependencies.Output('ges_graph', 'figure')
+    ],
     [dash.dependencies.Input('button', 'n_clicks')],
     getStates(modes))
 def update_output(n_clicks, *args):
@@ -81,7 +86,27 @@ def update_output(n_clicks, *args):
         if i%2 == 0:
             user_input[var] = float(args[i+1])
     ges = computeGes(user_input)
-    return f'Vos emmissions annuelles sont de {ges*365/1e3} kg de CO2.'
+    if ges > 0:
+        with open('transport_modes_ges.json', 'r') as f:
+            modes = json.load(f)
+        display = {'display': 'block'}
+        labels = []
+        values = []
+        for key in user_input:
+            if user_input[key] > 0:
+                labels += [modes[key]['name']]
+                values += [user_input[key] * modes[key]['value']]
+        figure = {
+            'data': [{'labels': labels, 'values': values, 'type': 'pie', 'name': 'ges'}],
+            'layout': { 'title': 'Vos emmissions de CO2.'}
+        }
+    else:
+        display = {'display': 'none'}
+        figure = {
+            'data': [{'labels': [], 'values': [], 'type': 'pie', 'name': ges}],
+            'layout': { 'title': 'Vos emmissions de CO2.'}
+        }
+    return f'Vos emmissions annuelles sont de {ges*365/1e3} kg de CO2.', display, figure
 
 
 if __name__ == '__main__':
