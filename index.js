78c1638712e96f4de87ba7109745a74f8110ef47
thomas-god <thomas.godlewski@icloud.com>
2020-02-03 08:48:24 +0100

    Revert "Fix const defined only in block scope"
    This reverts commit d154820818f614eb82a9945dcb0b0d3ab7da8f4f.

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const fs = require('fs');
 const clog = require('./custom-logger');
 
 // Prod (https) or dev (http)
-const httpsOptions = (process.env.GES_MODE === 'production') ?{
-  key: fs.readFileSync('/etc/letsencrypt/live/combiendecarbone.fr/privkey.pem', 'utf8'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/combiendecarbone.fr/fullchain.pem', 'utf8')
-}: {};
+if (process.env.GES_MODE === 'production') {
+  const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/combiendecarbone.fr/privkey.pem', 'utf8'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/combiendecarbone.fr/fullchain.pem', 'utf8')
+  };
+  
+}
 
 // Core app
 const app = express();
@@ -28,7 +31,7 @@ app.get('/', (req, res) => {
 // Prod (https) or dev (http)
 if (process.env.GES_MODE === 'production') {
   let port = 443;
-  const httpsServer = https.createServer(httpsOptions, app).listen(port, () => {
+  const httpsServer = https.createServer(options, app).listen(port, () => {
     console.log(`Server listenning in production mode on port ${port}...`);
   })
   
