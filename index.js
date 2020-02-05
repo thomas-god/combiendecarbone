4f31637fde4eaa0710f26d679fe8a85e6e4c4e62
thomas-god <thomas.godlewski@icloud.com>
2020-02-05 16:03:21 +0100

    Fix middlewares order close #47

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const options = {
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 
 const app = express();
-app.use(express.static('public'));
 app.use(morgan('combined', { stream: accessLogStream }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send("Hello world !");
