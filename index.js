fe58bfc3918d866a6833af7750f77249b14d5dfc
thomas-god <thomas.godlewski@icloud.com>
2020-02-10 12:34:01 +0100

    Remove https part from nodejs closes #56

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require("express");
-const https = require('https');
 const fs = require('fs');
 const morgan = require('morgan');
 const path = require('path');
-
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/combiendecarbone.fr/privkey.pem', 'utf8'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/combiendecarbone.fr/fullchain.pem', 'utf8')
-};
 
 // Write stream for logs
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
@@ -20,4 +14,4 @@ app.get('/', (req, res) => {
     res.send("Hello world !");
 })
 
-const httpsServer = https.createServer(options, app).listen(443)
+app.listen(8080, console.log("Server listening on port 8080..."));
