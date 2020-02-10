3b1e940349b8f8d8aa81f3a29459f061d2c8c612
thomas-god <thomas.godlewski@icloud.com>
2020-02-10 16:59:58 +0100

    Delete unimplemented form

--- public/scripts/logement.js
+++ public/scripts/logement.js
@@ -7,17 +7,19 @@ function Logement(div_id) {
 Logement.prototype.initDiv = function () {
     const div = document.getElementById(this.div_id);
     div.innerHTML = `
-    <h3> Votre consommation d'énergie</h3>
-        <label>
+    <h3>Logement et énergie</h3>
+        <p>Le secteur résidentiel-tertiaire est directement à l'origine d'environ <a href="https://www.statistiques.developpement-durable.gouv.fr/sites/default/files/2019-05/datalab-46-chiffres-cles-du-climat-edition-2019-novembre2018.pdf">17%</a> des émissions de gaz à effet de serre en France (la majorité pour nos besoins de chauffage), et ce sans compter les émissions indirectes dues à notre consommation d'électricité.</p>
+        <p>Le moyen le plus fiable d'estimer l'impact de notre consommation de <strong>gaz</strong> et <strong>d'électricité</strong> est d'utiliser les mesures utilisées dans nos factures d'énergie.<p>
+        <!--<label>
             Connaissez vous vos factures de gaz et d'électricité ?
             <select class="form-input" id="logement-select">
                 <option value="oui">Oui</option>
                 <option value="non">Non</option>
             </select>
-        </label>
+        </label>--!>
         
     <div id="logement-factures" class="logement-factures">
-        <p>Vos consommations: </p>
+        <h3>Vos consommations</h3>
         <ul>
         <li><label>
             D'électricité (MWh)
@@ -34,7 +36,8 @@ Logement.prototype.initDiv = function () {
     </div>
     `
     const select = document.getElementById("logement-select")
-    select.addEventListener("change", () => {
+    // Uncomment when computation not based on bills will be implemented
+/*     select.addEventListener("change", () => {
         var div_factures = document.getElementById("logement-factures")
         var div_modele = document.getElementById("logement-modele")
         if (select.value === "oui") {
@@ -44,26 +47,19 @@ Logement.prototype.initDiv = function () {
             div_factures.classList.add("disp-none")
             div_modele.classList.remove("disp-none")
         }
-    })
+    }) */
 }
 
 Logement.prototype.computeGES = function () {
     return new Promise((resolve, reject) => {
-        const select = document.getElementById("logement-select")
-        if (select.value === "oui") {
-            const input_elec = document.getElementById("logement-elec")
-            const input_gaz = document.getElementById("logement-gaz")
-            var elec = input_elec.value > 0 ? input_elec.value : 0
-            var gaz = input_gaz.value > 0 ? input_gaz.value : 0
-            var ges = {
-                "Électricité": Math.round(elec * 49.02), // kg CO2/MWh
-                "Gaz": Math.round(gaz * 234) // kg CO2/MWh
-            }
-            resolve({ name: "Logement", values: ges });
-        } else if (select.value === "non") {
-            resolve({ name: "Logement", values: 0 });
-        } else {
-            resolve({ name: "Logement", values: 0 });
+        const input_elec = document.getElementById("logement-elec")
+        const input_gaz = document.getElementById("logement-gaz")
+        var elec = input_elec.value > 0 ? input_elec.value : 0
+        var gaz = input_gaz.value > 0 ? input_gaz.value : 0
+        var ges = {
+            "Électricité": Math.round(elec * 49.02), // kg CO2/MWh
+            "Gaz": Math.round(gaz * 234) // kg CO2/MWh
         }
+        resolve({ name: "Logement", values: ges });
     })
 }
