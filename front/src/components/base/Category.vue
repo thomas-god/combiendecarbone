6dc9f8caeb763d20bca706bc51d4cda5438ac5dc
thomas-god <thomas.godlewski@icloud.com>
2020-03-25 15:10:19 +0100

    Base component for Category form

---
+++ front/src/components/base/Category.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-card class="mx-auto my-3 pa-3" max-width="650px">
+    <v-card-text class="text-justify">
+      <slot name="text">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores
+          est exercitationem repudiandae ab harum libero atque, ratione,
+          provident eos iure laboriosam, commodi reprehenderit veritatis nostrum
+          minima quibusdam sint dolores. Laboriosam.
+        </p>
+      </slot>
+    </v-card-text>
+    <v-btn color="primary" dark @click="form = true">
+      {{ form_touched ? btn_name.dirty : btn_name.clean }}
+    </v-btn>
+    <v-dialog
+      v-model="form"
+      max-width="550px"
+      @click:outside="form = false"
+      class="ma-0"
+    >
+      <slot name="form" :close="closeForm">
+        <v-form></v-form>
+      </slot>
+    </v-dialog>
+    <slot name="card" :touched="form_touched">
+      <v-card>
+        <v-card-title>Titre de la carte</v-card-title>
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ea
+          molestiae alias impedit aspernatur eaque, voluptates fuga et quod
+          architecto eos nulla totam sed, fugiat eius laudantium nostrum
+          provident nemo.
+        </v-card-text>
+      </v-card>
+    </slot>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    btn_name: {
+      type: Object,
+      default() {
+        return {
+          dirty: 'Modifier',
+          clean: 'Remplir'
+        }
+      }
+    }
+  },
+  data() {
+    return {
+      form: false,
+      form_touched: false
+    }
+  },
+  methods: {
+    closeForm() {
+      this.form = false
+      this.form_touched = true
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ front/src/components/base/ExampleCat.vue
@@ -0,0 +1,29 @@
+<template>
+  <def-cat>
+    <template v-slot:text>
+      <h1>Toto</h1>
+    </template>
+
+    <template v-slot:form="{ close }">
+      <v-card>
+        <v-form>
+          <v-select
+            :items="['Oui', 'Non']"
+            label="Je connais mes factures d'énergie"
+            required
+          ></v-select>
+          <v-btn @click="close">OK</v-btn>
+        </v-form>
+      </v-card>
+    </template>
+  </def-cat>
+</template>
+
+<script>
+import defCat from './Category.vue'
+export default {
+  components: { defCat }
+}
+</script>
+
+<style></style>
