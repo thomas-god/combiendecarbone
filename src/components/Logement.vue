9832fc35900fbf7042209d9a7ebd0bb465c2f396
thomas-god <thomas.godlewski@icloud.com>
2020-05-28 14:07:17 +0200

    Ajout d'un formulaire logement si pas de factures closes #67

--- src/components/Logement.vue
+++ src/components/Logement.vue
@@ -18,43 +18,26 @@
     </template>
 
     <template v-slot:card>
-      <v-card max-width="550px" class="mx-auto my-3">
-        <v-card-title v-show="factures.flag === 'Oui'">
-          Votre consommation
-        </v-card-title>
-        <v-card-text class="text-left" v-show="factures.flag === 'Oui'">
-          <p>Électricité: {{ factures.elec === '' ? 0 : factures.elec }} MWh</p>
-          <p>Gaz: {{ factures.gaz === '' ? 0 : factures.gaz }} MWh</p>
-        </v-card-text>
-      </v-card>
+      <logement-card />
     </template>
   </category>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
 import Category from './base/Category.vue'
 import LogementForm from './LogementForm.vue'
+import logementCard from './LogementCard.vue'
 
 export default {
   components: {
     LogementForm,
+    logementCard,
     Category
   },
   data() {
     return {
       form: false,
       form_touched: false
-    }
-  },
-  computed: {
-    factures: {
-      get() {
-        return this.$store.state.logement.factures
-      },
-      set(value) {
-        mapActions('logement/updateFactures', value)
-      }
     }
   },
   methods: {
