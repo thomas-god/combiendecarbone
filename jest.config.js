84b681566a27c0ed3c14c336e945309c5040a023
thomas-god <thomas.godlewski@icloud.com>
2020-07-22 14:15:33 +0200

    Update config file for tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: false,
+  collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**'],
+  coverageReporters: ['html', 'text-summary']
 }
