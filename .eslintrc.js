8b0d34d9a5e36fd111191380e310baa3463ba42a
thomas-god <thomas.godlewski@icloud.com>
2020-08-26 17:07:04 +0200

    Maintenance of dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,8 @@ module.exports = {
 
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    camelcase: ['error', { properties: 'never' }]
   },
 
   parserOptions: {
