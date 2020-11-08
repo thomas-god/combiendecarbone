e85d88db2f4e5e2dc7bd3177c2b82a808b38f612
thomas-god <thomas.godlewski@icloud.com>
2020-11-08 20:27:10 +0100

    Add ecogeste component for public services

---
+++ src/components/ecogestes/EcogesteServicesPublics.vue
@@ -0,0 +1,41 @@
+<template>
+  <ecogeste-popup v-model="popup">
+    <template v-slot:title>
+      Les émissions des services publics 🏥
+    </template>
+
+    <template v-slot:text>
+      <div>
+        <v-card-text class="text-body-1 text-left pt-0">
+          Si nous ne pouvons pas directement réduire les émissions liées aux
+          services publics que nous utilisons (santé, éducation, police,
+          justice, etc.), nous pouvons quand même avoir un impact, avec notre
+          bulletin de vote, mais aussi en s'impliquant et en participant dès que
+          possible (concertations publiques, appels à projets, budgets
+          participatifs, etc.).
+        </v-card-text>
+      </div>
+    </template>
+  </ecogeste-popup>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import EcogesteBase from './EcogesteBase.vue'
+
+export default Vue.extend({
+  extends: EcogesteBase
+})
+</script>
+
+<style scoped>
+li {
+  font-size: 1rem !important;
+  color: rgba(0, 0, 0, 0.6) !important;
+  text-align: start !important;
+}
+
+.ecogestes_liens div.v-card__text  {
+  padding-top: Opx;
+}
+</style>
