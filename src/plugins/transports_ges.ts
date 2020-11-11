f0fd47887e7aad7bdf8377aa370ee7781705005a
thomas-god <thomas.godlewski@icloud.com>
2020-11-11 08:45:45 +0100

    Add utils functions to check if a travel is compliant

--- src/plugins/transports_ges.ts
+++ src/plugins/transports_ges.ts
@@ -28,6 +28,72 @@ export interface Travel {
   ecogeste?: Ecogeste
 }
 
+/**
+ * Check if an object is compatible with the Travel interface.
+ * @param travel Travel object to check.
+ */
+export function checkTravel(travel: Travel): boolean {
+  /**
+   * Name.
+   */
+  if (travel.name === undefined) return false
+  if (typeof travel.name !== 'string') return false
+
+  /**
+   * Departure and arrival places.
+   */
+  if (!checkPlace(travel.departure)) return false
+  if (!checkPlace(travel.arrival)) return false
+
+  /**
+   * Travel mode and type.
+   */
+  if (travel.mode === undefined) return false
+  if (typeof travel.mode !== 'string') return false
+  if (travel.type === undefined) return false
+  if (!['Régulier', 'Occasionnel'].includes(travel.type)) return false
+
+  /**
+   * Roundabout trip.
+   */
+  if (travel.ar === undefined) return false
+  if (typeof travel.ar !== 'boolean') return false
+
+  /**
+   * Passengers and frequency.
+   */
+  if (travel.passengers === undefined) return false
+  if (typeof travel.passengers !== 'number') return false
+  if (travel.freq === undefined) return false
+  if (typeof travel.freq !== 'number') return false
+
+  return true
+}
+
+/**
+ * Check if an object is compatible with the PlaceForm interface.
+ * @param place Place object to check.
+ */
+export function checkPlace(place: PlaceForm): boolean {
+  /**
+   * Name and placeholder.
+   */
+  if (place.name === undefined) return false
+  if (typeof place.name !== 'string') return false
+  if (place.placeholder === undefined) return false
+  if (typeof place.placeholder !== 'string') return false
+
+  /**
+   * Latitude and longitude.
+   */
+  if (place.lat === undefined) return false
+  if (typeof place.lat !== 'number') return false
+  if (place.lng === undefined) return false
+  if (typeof place.lng !== 'number') return false
+
+  return true
+}
+
 export interface Distance {
   distance: number
   mode: string
@@ -76,7 +142,11 @@ enum modes_ges {
   Avion = 249.6
 }
 
-function computeGes(travel: Travel): Travel {
+/**
+ * Compute total GES émissions of the travel.
+ * @param travel Travel object.
+ */
+export function computeGes(travel: Travel): Travel {
   let ges = 0
   if (travel.distances && travel.distances.length > 0) {
     for (let i = 0; i < travel.distances.length; i++) {
@@ -94,6 +164,10 @@ function computeGes(travel: Travel): Travel {
   return travel
 }
 
+/**
+ * Choose the travel corresponding ecogeste.
+ * @param travel Travel item
+ */
 function chooseEcogeste(travel: Travel): Ecogeste {
   const ecogeste: Ecogeste = { name: '' }
 
@@ -127,5 +201,3 @@ function chooseEcogeste(travel: Travel): Ecogeste {
 
   return ecogeste
 }
-
-export { computeGes }
