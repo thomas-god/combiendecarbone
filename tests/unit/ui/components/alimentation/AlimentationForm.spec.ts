6092c94194b569545528df64a343ad3c44212b6f
thomas-god <thomas.godlewski@icloud.com>
2022-03-14 18:31:07 +0100

    feat(food): fix test on food form

--- tests/unit/ui/components/alimentation/AlimentationForm.spec.ts
+++ tests/unit/ui/components/alimentation/AlimentationForm.spec.ts
@@ -1,6 +1,6 @@
 import AlimentationForm from "@/ui/components/alimentation/AlimentationForm.vue";
 import { renderWithVuetify } from "@/../tests/setup";
-import { fireEvent } from "@testing-library/vue";
+import { fireEvent, waitFor } from "@testing-library/vue";
 import { NewFoodRegimeErrors } from "@/domain/usecases/NewFoodRegime";
 
 const foodModule = {
@@ -8,7 +8,7 @@ const foodModule = {
 };
 
 describe("Food form", () => {
-  it("should display missing fields error messages when tryign to validate the form", () => {
+  it("should display missing fields error messages when tryign to validate the form", async () => {
     const { getAllByText, getByText } = renderWithVuetify(AlimentationForm, {
       props: { foodModule: foodModule },
     });
@@ -16,7 +16,9 @@ describe("Food form", () => {
     const validateBtn = getByText("Ok");
     fireEvent.click(validateBtn);
 
-    const items = getAllByText(NewFoodRegimeErrors.MISSING_VALUE);
-    expect(items.length).toEqual(4);
+    await waitFor(() => {
+      const items = getAllByText(NewFoodRegimeErrors.MISSING_VALUE);
+      expect(items.length).toEqual(4);
+    });
   });
 });
